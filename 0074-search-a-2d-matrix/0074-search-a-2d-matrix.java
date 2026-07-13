a5c254e7aed881e0f52a8d059889e2c850956c27
Kavimugil Rajasekar <kavimugil28@gmail.com>
2026-07-13 20:58:46 +0530

    Time: 0 ms (100%), Space: 44.1 MB (8.69%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,52 @@
+class Solution {
+    private int findColumn(int[][] matrix, int target) {
+        int left = 0;
+        int right = matrix.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (matrix[mid][0] == target) {
+                return mid;  // Return the row index
+            } else if (matrix[mid][0] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return right;  // Returns the row where target would be inserted
+    }
+
+    private boolean findRow(int[] row, int target) {
+        int left = 0;
+        int right = row.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (row[mid] == target) {
+                return true;
+            } else if (row[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+    
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        
+        int row = findColumn(matrix, target);
+        
+        // If row is -1, target is smaller than all first elements
+        if (row < 0) {
+            return false;
+        }
+        
+        return findRow(matrix[row], target);
+    }
+}
