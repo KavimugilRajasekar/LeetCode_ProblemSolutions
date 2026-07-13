71f4271f0715ccf599fd8386c1abccf845c1ac8c
Kavimugil Rajasekar <kavimugil28@gmail.com>
2026-07-13 19:23:40 +0530

    Time: 4 ms (90.75%), Space: 82.8 MB (69.4%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -4,12 +4,15 @@ class Solution {
         int fast = nums[0];
 
         //Create a Cycle First !! 
+        // Slow Moves 1 a Step at a Time !!
+        // Fast Moves 2 a Step at a Time !!
         do{
             slow = nums[slow];
             fast = nums[nums[fast]];
         }while(slow != fast);
 
-        //Find the Enterence of the Cycle of Created using 
+        //Find the Enterence of the Cycle of Created using
+        // Now both the Slow as well Fast Moves 1 a Step at a Time !! 
         slow = nums[0];
         while(slow != fast){
             slow = nums[slow];
