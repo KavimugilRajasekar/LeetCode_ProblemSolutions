d13085081d0712d672a48ed1b97e1872b00ece2a
Kavimugil Rajasekar <kavimugil28@gmail.com>
2026-08-19 08:12:58 +0530

    Time: 1 ms (28.42%), Space: 43.1 MB (52.28%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,6 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-       
+       // Finding the last Index wont be the correct answer but , Split them and find the last Index and Find its Length !! 
         return s.split(" ")[s.split(" ").length - 1].length();
         
     }
